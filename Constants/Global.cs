8b81754dbe8b1ace22112263fd9858186406bc0f
Joan Alexander <106604262+DevArv@users.noreply.github.com>
2025-06-07 22:00:43 -0400

    Version update.

--- Constants/Global.cs
+++ Constants/Global.cs
@@ -2,5 +2,10 @@ namespace FlightPlanner.Constants;
 
 public static class Global
 {
+    //Software version
+    public static string VERSION = "1.0.0";
+    public static string VERSION_DATE = "JUNE 07 2024";
+    
+    //Software defaults values
     public static string DATETIME_FORMAT = "yyyy-MM-dd";
 }
